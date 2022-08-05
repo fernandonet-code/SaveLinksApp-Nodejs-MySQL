19304cdafa07999a356fb7eb83e25f34c27a6198
Fernando Alvarez <fernandoalexisalvarezruiz@gmail.com>
2022-08-05 15:26:13 -0500

    First commit

--- database/database.sql
+++ database/database.sql
@@ -36,16 +36,6 @@ CREATE TABLE `links` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `links`
---
-
-LOCK TABLES `links` WRITE;
-/*!40000 ALTER TABLE `links` DISABLE KEYS */;
-INSERT INTO `links` VALUES (16,'Facebook','https://www.facebook.com','Social network',NULL,'2022-07-04 16:34:04'),(19,'Wikipedia','https://www.wikipedia.com','Articles',NULL,'2022-07-04 22:00:26'),(21,'Facebook','https://www.facebook.com','Social network',1,'2022-07-07 20:31:37'),(22,'Wikipedia','https://www.wikipedia.com','Articles',3,'2022-07-07 20:32:07'),(23,'Wikipedia','https://www.wikipedia.com','Articles online',1,'2022-07-07 21:05:56');
-/*!40000 ALTER TABLE `links` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `sessions`
 --
 
@@ -59,16 +49,6 @@ CREATE TABLE `sessions` (
   PRIMARY KEY (`session_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `sessions`
---
-
-LOCK TABLES `sessions` WRITE;
-/*!40000 ALTER TABLE `sessions` DISABLE KEYS */;
-INSERT INTO `sessions` VALUES ('Wf6d9XCfMio_GjwCoJicl4g2PXvF93DT',1657314573,'{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"httpOnly\":true,\"path\":\"/\"},\"flash\":{}}');
-/*!40000 ALTER TABLE `sessions` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
@@ -85,17 +65,6 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'fernandonet.code','$2a$10$p17MEu8QLekZWkGXBnNFjukng.6oXaASt3xSzf3hb9/nsH7k3eEFS','Fernando Alvarez'),(2,'fernandonet.code','$2a$10$8K3lRdvDDi2.M5Yw5bTKP.xt8tsxevpT4EZ8XHLi8VB2KeT9Wy4z.','Fernando Alvarez'),(3,'Erika','$2a$10$O7AEiOEFgMnHh8gpTZkSG.klXUKREYFTT6UBWpCPl1n8G5DK4Nz/.','Erika Sanchez');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
