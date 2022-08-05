4e7de0be37003ef6b03e373f4c393ec92f1dce55
Fernando Alvarez <fernandoalexisalvarezruiz@gmail.com>
2022-08-05 16:13:10 -0500

    First commit

--- database/database.sql
+++ database/database.sql
@@ -15,7 +15,9 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE DATABASE database_links;
+DROP DATABASE IF EXISTS `database_links`;
+CREATE DATABASE `database_links`;
+USE `database_links`; 
 
 --
 -- Table structure for table `links`
